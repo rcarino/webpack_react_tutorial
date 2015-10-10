84cd7d113c5e78e51fd51bf56879f6b2152c87d0
Raymond Carino <sloooooowduck@gmail.com>
2015-10-10 16:24:15 -0700

    Add webpack starter initial files

---
+++ app/component.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Hello worlds z0mg this is awesome';
+
+    return element;
+};
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var path = require('path');
+var webpack = require('webpack');
+
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+    entry: APP_PATH,
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: APP_PATH
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlwebpackPlugin({title: 'Kanban app'})
+    ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
